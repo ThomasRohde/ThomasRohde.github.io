40f3906cc123fb4d40edb4014d944b55d2245262
Thomas Klok Rohde <thomas.rohde@danskebank.dk>
2024-07-08 15:41:42 +0200

    Create PWA

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,33 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import viteBlogPlugin from './vite-blog-plugin'
 
 export default defineConfig({
-  plugins: [react(), viteBlogPlugin()],
+  plugins: [react(), viteBlogPlugin(),
+  VitePWA({
+    registerType: 'autoUpdate',
+    includeAssets: ['favicon.ico', 'icon-192x192.png', 'icon-512x512.png'],
+    manifest: {
+      name: 'Thomas Klok Rohde Portfolio',
+      short_name: 'TKR Portfolio',
+      description: 'Portfolio and blog of Thomas Klok Rohde',
+      theme_color: '#1976d2',
+      icons: [
+        {
+          src: '/icon-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: '/icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    }
+  })
+  ],
   base: '/',
   server: {
     watch: {
